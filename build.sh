7928a63e2607e3c2f9d260b606d9554a6445fc7b
gtd <gtd@pintuit>
2022-07-19 16:31:18 +0000

    add deployment script, Dockerfile, and unit-config

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build --build-arg API_URL=https://gtdxxiv-service.pintugtd.com/api/ . -t registry.pintusingapura.org/gtd/gtd-xxiv-frontend:latest
+docker push registry.pintusingapura.org/gtd/gtd-xxiv-frontend:latest
